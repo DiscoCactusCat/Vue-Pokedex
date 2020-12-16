5b9279529c5ac83b14491d600d5c4a477ea44035
Caroline Roy <caroline.roy.tln@gmail.com>
2020-12-16 11:55:20 +0100

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Hello Vue 3</h1>
+</template>
+
+<script>
+    export default{
+        name: 'Home',
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        },
+        computed:{
+
+        },
+        mounted(){
+            console.log("Home is mounted");
+        }
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
